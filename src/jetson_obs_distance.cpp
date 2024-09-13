d34e84535eef4480e9de4bbd65c48bb22cdd66fe
dirksavage <dirksavage88@gmail.com>
2024-09-13 18:33:00 +0000

    fix to timestamping and min distance
    Signed-off-by: dirksavage <dirksavage88@gmail.com>

--- src/jetson_obs_distance.cpp
+++ src/jetson_obs_distance.cpp
@@ -8,7 +8,6 @@
 #include <rclcpp/rclcpp.hpp>
 #include <px4_msgs/msg/distance_sensor.hpp>
 #include <px4_msgs/msg/obstacle_distance.hpp>
-#include <px4_msgs/msg/timesync_status.hpp>
 
 class JetsonAvoidance :  public rclcpp::Node 
 {
@@ -29,11 +28,6 @@ class JetsonAvoidance :  public rclcpp::Node
     // Fill the publisher
     _obs_distance_msg = this->create_publisher<px4_msgs::msg::ObstacleDistance>("/fmu/in/obstacle_distance", 10);
 
-    _timesync_msg = this->create_subscription<px4_msgs::msg::TimesyncStatus>("/fmu/out/timesync_status", qos, std::bind(&JetsonAvoidance::TimeSyncCB, this, std::placeholders::_1));
-    
-    // Always have a backup in case we aren't timesynced to PX4 time
-    _sensor_time_start = std::chrono::steady_clock::now();
-    
     // Get the i2c bus
     _i2c_bus = this->get_parameter("i2c_bus").as_int();
     
@@ -47,7 +41,7 @@ class JetsonAvoidance :  public rclcpp::Node
     // only create wall timer if sensor init was successful
      if(_return_status == 0) {
      
-    	_timer = this->create_wall_timer(std::chrono::milliseconds(15), std::bind(&JetsonAvoidance::DistanceReadCB, this));
+    	_timer = this->create_wall_timer(std::chrono::milliseconds(10), std::bind(&JetsonAvoidance::DistanceReadCB, this));
      }
 
   }
@@ -56,32 +50,21 @@ class JetsonAvoidance :  public rclcpp::Node
 
     void SensorInit(void);
     void DistanceReadCB(void);
-    void TimeSyncCB(px4_msgs::msg::TimesyncStatus::UniquePtr msg);
     
     // Create publisher
     rclcpp::Publisher<px4_msgs::msg::ObstacleDistance>::SharedPtr _obs_distance_msg;
     
-    // Create subscriber to PX4 timesync messages
-    rclcpp::Subscription<px4_msgs::msg::TimesyncStatus>::SharedPtr _timesync_msg;
-    
-    // Create Backup Timer
-    rclcpp::TimerBase::SharedPtr _timer;
-    
-    // Timestamp of the node starting
-    std::chrono::_V2::steady_clock::time_point _sensor_time_start; 
-
     // VL53L5CX configuration & results struct
     VL53L5CX_Configuration _config;
-    
     VL53L5CX_ResultsData _results;
+    // Create Timer
+    rclcpp::TimerBase::SharedPtr _timer;
     
     // Constants
     const uint16_t _UINT16_MAX{65535};
     const uint8_t _OBS_DISTANCE_MAX{72};
     
     // Member variables
-    bool _synced;
-    uint64_t _synced_ts;
     uint8_t _return_status{0};
     uint8_t _is_active{0};
     uint8_t _data_ready{0};
@@ -160,7 +143,6 @@ void JetsonAvoidance::SensorInit()
 	// Set the ranging mode to continuous (VCSEL always on)
 	_return_status |= vl53l5cx_set_ranging_mode(&_config, VL53L5CX_RANGING_MODE_CONTINUOUS);
 	// Integration ignored if in continuous
-	//_return_status |= vl53l5cx_set_integration_time_ms(&_config, 60);
         _return_status |= vl53l5cx_set_ranging_frequency_hz(&_config, 5);
         if(_return_status != 0) {
           RCLCPP_ERROR(this->get_logger(), "ERROR: VL53L5CX frequency not set");
@@ -205,16 +187,10 @@ void JetsonAvoidance::SensorInit()
 
 }
 
-void JetsonAvoidance::TimeSyncCB(const px4_msgs::msg::TimesyncStatus::UniquePtr msg) {
-  _synced_ts = msg->timestamp;
-  _synced = true;  
-
-}
 void JetsonAvoidance::DistanceReadCB() {
   uint16_t distance_val;
   uint8_t distance_indx = 0;
   px4_msgs::msg::ObstacleDistance obs;
-  auto sensor_time_end = std::chrono::steady_clock::now();
 
   vl53l5cx_check_data_ready(&_config, &_data_ready);
   
@@ -306,17 +282,8 @@ void JetsonAvoidance::DistanceReadCB() {
       //RCLCPP_INFO(this->get_logger(), "Range: %hu", distance_val);
     } // End for
 
-    // Check if we are subscribed to PX4 timesync
-    if (_synced) {
-        obs.timestamp = _synced_ts;
-    } else {
-        // Fall back on internal timer based on when the sensor node was started
-    	obs.timestamp = std::chrono::duration_cast<std::chrono::microseconds>(sensor_time_end - _sensor_time_start).count();
+    	obs.timestamp = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::steady_clock::now()).time_since_epoch().count();
   
-        RCLCPP_WARN(this->get_logger(), "WARNING: Not time synced with FMU");
-    }
-
-
     // In 8x8 ranging mode the FoV is ~63 degrees, each of the 8 elements increment is 10 deg to divide by 360 evenly
     obs.angle_offset = -40.0f;
     obs.increment = 10;
@@ -325,12 +292,10 @@ void JetsonAvoidance::DistanceReadCB() {
     obs.frame = 12; // MAV_FRAME_BODY_FRD
 
     // Sensor limits in centimeters
-    obs.min_distance = _UINT16_MAX;
+    obs.min_distance = 5;
     obs.max_distance = 400;
     _obs_distance_msg->publish(obs);
 
-    // reset time sync in the event we lose timesync from px4
-    _synced = false;
 
   } // End if
 
